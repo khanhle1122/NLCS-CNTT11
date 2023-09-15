2806678a620230dc92825a1b661a84e7fa7ae8d4
khanh le <khanhb2014751@student.ctu.edu.vn>
2023-09-15 10:11:32 +0700

    commit

---
+++ resources/views/photo.blade.php
@@ -0,0 +1,16 @@
+@include('layouts.header')
+
+<main>
+    <div class="container ">
+        <div class="row ">
+            <div class="px-0  col-md-6 offset-sm-3 mt-5 ">
+                <form  class="shadow-sm p-4 mb-4 bg-white">
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+@include('layouts.footer')
